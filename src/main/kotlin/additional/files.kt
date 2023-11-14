d3d3ab8845ad1008ef20b8b636d90d712ec2ab1c
PozyalovMihail <pozyalov@gmail.com>
2023-11-14 21:28:34 +0300

    Coursework-Stage3 fix.

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -31,8 +31,7 @@ fun main() {
         when(answer) {
             "1" -> println("Вы нажали 1")
             "2" -> {
-                var learnedWordsCount: Int
-                learnedWordsCount = dictionary.filter { it.correctAnswersCount >= 3 }.count()
+                val learnedWordsCount = dictionary.filter { it.correctAnswersCount >= 3 }.count()
                 println("Выучено $learnedWordsCount из ${dictionary.count()} слов | ${100 / dictionary.count() * learnedWordsCount}%")
             }
             "0" -> {
