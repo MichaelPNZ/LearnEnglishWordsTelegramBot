b04a728dd76c9e841165b9dad009333b74ae9374
PozyalovMihail <pozyalov@gmail.com>
2023-11-15 11:19:52 +0300

    Coursework-Stage4 fix.

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -29,13 +29,48 @@ fun main() {
         println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
         val answer = readln()
         when(answer) {
-            "1" -> println("Вы нажали 1")
+            "1" -> {
+                while (true) {
+                    val unlearnedWords = mutableListOf<String>()
+
+                    // Наполняем список невыученных слов
+                    dictionary.forEach { if (it.correctAnswersCount < 3) unlearnedWords.add(it.original) }
+
+                    val wordForQuestion = unlearnedWords.shuffled().firstOrNull() // Слово для вопроса/правильный ответ
+
+                    // Проверяем есть ли невыученные слова
+                    if (unlearnedWords.isEmpty()) {
+                        println("Вы выучили все слова!")
+                        return
+                    }
+
+                    println("Выберите правильный первеод слова: $wordForQuestion или нажмите 0 для выхода в главное меню")
+
+                    // Печатаем список рандомных ответов
+                    unlearnedWords.take(4).shuffled().forEachIndexed { index, element ->  println("${index + 1}: $element")  }
+
+                    val answer1 = readln()
+
+                    when(answer1) {
+                        "1" -> println("Вы нажали 1")
+                        "2" -> println("Вы нажали 2")
+                        "3" -> println("Вы нажали 3")
+                        "4" -> println("Вы нажали 4")
+                        "0" -> {
+                            println("Выходим в главное меню")
+                            break
+                        }
+                        else -> println("неверное нажатие")
+                    }
+
+                }
+            }
             "2" -> {
                 val learnedWordsCount = dictionary.filter { it.correctAnswersCount >= 3 }.count()
                 println("Выучено $learnedWordsCount из ${dictionary.count()} слов | ${100 / dictionary.count() * learnedWordsCount}%")
             }
             "0" -> {
-                println("Вы нажали 0")
+                println("Вы нажали: Выход")
                 return
             }
             else -> println("неверное нажатие")
