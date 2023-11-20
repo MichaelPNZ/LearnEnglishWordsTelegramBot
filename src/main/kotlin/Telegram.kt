71864db6f76817a972e6e320893764e7500cf5c1
PozyalovMihail <pozyalov@gmail.com>
2023-11-20 17:29:08 +0300

    Coursework-Stage8 completed.

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,32 +1,35 @@
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
-
 fun main(args: Array<String>) {
 
+    val telegramBotService = TelegramBotService()
     val botToken = args[0]
     var updateId = 0
+    var chatId = 0
 
     while (true) {
         Thread.sleep(2000)
-        val updates: String = getUpdates(botToken, updateId)
+        val updates: String = telegramBotService.getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        updateId = updateIdString.toInt() + 1
+        val messageRegex = toRegex("\"text\":\"(.+?)\"", updates)
+        val updateIdRegex = toRegex("\"update_id\":(\\d+)", updates)
+        val chatIdRegex = toRegex("\"id\":(\\d+)", updates)
+
+        if (updateIdRegex != null) updateId = updateIdRegex.toInt() + 1
+        if (chatIdRegex != null) chatId = chatIdRegex.toInt()
+
+        if (messageRegex != null && messageRegex == "Hello") {
+            val sendMessage: String = telegramBotService.sendMessage(botToken, chatId.toString(), "Hello")
+            println(sendMessage)
+        }
+
     }
 
 }
 
-fun getUpdates(botToken: String, updateId: Int): String {
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
-    val client: HttpClient = HttpClient.newBuilder().build()
-    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
-    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+fun toRegex(query: String, updates: String): String? {
+    val messageTextRegex: Regex = query.toRegex()
+    val matchResult: MatchResult? = messageTextRegex.find(updates)
+    val group = matchResult?.groups
 
-    return response.body()
+    return group?.get(1)?.value
 }
